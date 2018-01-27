87f2895220807f607e4715ea8428f57f8e4cca29
Stachu <sjwilczynski@gmail.com>
2018-01-27 23:05:27 +0000

    cleaup in loader, add two ideas to list

--- loader.py
+++ loader.py
@@ -6,9 +6,11 @@ from threes import *
 
 
 class Loader(object):
-    def __init__(self, game=Threes, epsilon=0.01):
-        self.game = game
-        self.game_state = self.game()
+    def __init__(self, game=None, epsilon=0.1):
+        if game is not None:
+            self.game = game
+        else:
+            self.game = Threes()
         self.epsilon = epsilon
 
     def get(self, model, batch_size=20):
@@ -18,25 +20,12 @@ class Loader(object):
         return np.array(self.replay_memory)
 
     def _getOneBatch(self, model):
-        if not self.game_state.getPossibleMoves():
-            self.game_state = self.game()
+        if not self.game.getPossibleMoves():
+            self.game.newGame()
         move = None
         if random.random() < self.epsilon:
-            move = random.choice(self.game_state.getPossibleMoves())
+            move = random.choice(self.game.getPossibleMoves())
         else:
-            res = model.Q(self.game_state.data(True))
-            move = sorted(self.game_state.getPossibleMoves(), key=lambda x: res[x.value])[0]
-        return self.game_state.getTransitionData(move, True, True)
-
-
-if __name__ == u'__main__':
-    class f:
-        def Q(self, a):
-            return [a[0][-1]]
-
-
-    loa = Loader()
-    print
-    random.choice(loa.game_state.getPossibleMoves())
-    print
-    loa.get(f())
+            res = model.Q(self.game.data(True))
+            move = sorted(self.game.getPossibleMoves(), key=lambda x: res[x.value])[0]
+        return self.game.getTransitionData(move, True, True)
