ccd1b9de302b8c8ce92c5a0d6fe4a0a21f0fd6ea
Stachu <sjwilczynski@gmail.com>
2018-01-27 21:08:43 +0000

    added replay memory

---
+++ replayMemory.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+entry_len = 40
+class ReplayMemory(object):
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.data = np.zeros((1,entry_len))
+
+    def add(self, new_data):
+        self.data = np.append(self.data, new_data, axis=0)
+        to_cut = self.data.shape[0] - self.max_size
+        if to_cut > 0:
+            self.data = self.data[to_cut:, :]
+    
+    def choose(self, how_many):
+        indices = np.random.randint(1, len(self.data), how_many)
+        return self.data[indices, :]
